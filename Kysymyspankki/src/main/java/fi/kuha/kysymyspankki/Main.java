a7b9f5a86f2d844d09d85836b41497dd8dedd642
Harviainen Juha A M <hjuha@da5-cs-b221-13.pc.helsinki.fi>
2018-10-10 12:58:08 +0300

    Add Thymeleaf

--- Kysymyspankki/src/main/java/fi/kuha/kysymyspankki/Main.java
+++ Kysymyspankki/src/main/java/fi/kuha/kysymyspankki/Main.java
@@ -1,13 +1,18 @@
 package fi.kuha.kysymyspankki;
 
+import java.util.HashMap;
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         Database db = new Database("jdbc:sqlite:kysymyspankki.db");
         
         Spark.get("/", (req, res) -> {
-            return "Hei maailma!";
-        });
+            HashMap map = new HashMap<>();
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
     }
 }
