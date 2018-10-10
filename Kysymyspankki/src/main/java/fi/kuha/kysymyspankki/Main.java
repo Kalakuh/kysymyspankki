daf3ac455598ae026f509158eb439f7553c1f2e0
Harviainen Juha A M <hjuha@da5-cs-b221-13.pc.helsinki.fi>
2018-10-10 12:53:07 +0300

    Add Dao and Database

--- Kysymyspankki/src/main/java/fi/kuha/kysymyspankki/Main.java
+++ Kysymyspankki/src/main/java/fi/kuha/kysymyspankki/Main.java
@@ -3,7 +3,9 @@ package fi.kuha.kysymyspankki;
 import spark.Spark;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Database db = new Database("jdbc:sqlite:kysymyspankki.db");
+        
         Spark.get("/", (req, res) -> {
             return "Hei maailma!";
         });
