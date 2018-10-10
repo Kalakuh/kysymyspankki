9e7234c66e097e3b4860c996066e7070c99f79ee
Harviainen Juha A M <hjuha@da5-cs-b221-13.pc.helsinki.fi>
2018-10-10 12:37:51 +0300

    Add Hello World webpage

--- Kysymyspankki/src/main/java/fi/kuha/kysymyspankki/Main.java
+++ Kysymyspankki/src/main/java/fi/kuha/kysymyspankki/Main.java
@@ -1,7 +1,11 @@
 package fi.kuha.kysymyspankki;
 
+import spark.Spark;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Spark.get("/", (req, res) -> {
+            return "Hei maailma!";
+        });
     }
 }
